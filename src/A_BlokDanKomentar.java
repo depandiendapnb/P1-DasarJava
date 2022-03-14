72f927339a81f8329813e709167ab94543c14964
Depandi Enda <depandienda@gmail.com>
2022-03-14 14:53:07 +0700

    Initial project pertemuan 1 dasar Java

---
+++ src/A_BlokDanKomentar.java
@@ -0,0 +1,12 @@
+public class A_BlokDanKomentar {
+    public static void main(String[] args) {
+        System.out.println("Pemrograman Berorientasi Objek dengan Java");
+        //contoh komentar 1 baris
+        /*
+        Contoh komentar lebih dari 1 baris
+         */
+        /**
+         * Java Doc --> baris ini untuk mendokumentasikan kode program
+         */
+    }
+}
