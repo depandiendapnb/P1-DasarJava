6c5ff34cf0c71f87c3bcc4e6e3266ba8ea197dea
Depandi Enda <depandienda@gmail.com>
2022-03-14 15:15:46 +0700

    Percobaan Whitespace di Java

---
+++ src/B_BlokDanKomentar.java
@@ -0,0 +1,4 @@
+/**
+ * Contoh penggunaan whitespace di Java
+ */
+public class B_BlokDanKomentar { public static void main(String[] args) { System.out.println("Pemrograman berorientasi objek dengan Java"); } }
